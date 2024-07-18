7cabad524dcdb2be658044a6d61a7504798d7617
AplicacionesMoviles2 <dam2.idat2024@gmail.com>
2024-07-17 20:41:36 -0500

    consumo de API Rest jsonplaceholder

--- app/src/main/java/pe/idat/appmvvmrest/posts/view/PostScreen.kt
+++ app/src/main/java/pe/idat/appmvvmrest/posts/view/PostScreen.kt
@@ -1,4 +1,15 @@
 package pe.idat.appmvvmrest.posts.view
 
-class PostScreen {
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun postScreen(postViewModel: PostViewModel){
+    Box(modifier = Modifier.fillMaxSize().padding(8.dp)){
+        postViewModel.getPosts()
+    }
 }
--- app/src/main/java/pe/idat/appmvvmrest/posts/view/PostViewModel.kt
+++ app/src/main/java/pe/idat/appmvvmrest/posts/view/PostViewModel.kt
@@ -1,4 +1,27 @@
 package pe.idat.appmvvmrest.posts.view
 
-class PostViewModel {
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import pe.idat.appmvvmrest.posts.domain.GetPostUseCase
+import javax.inject.Inject
+
+@HiltViewModel
+class PostViewModel @Inject constructor(private val getPostUseCase: GetPostUseCase)
+    : ViewModel() {
+
+    private val _postList = MutableLiveData<String>()
+    val postList: LiveData<String> = _postList
+
+    fun getPosts(){
+        viewModelScope.launch {
+            Log.i("POSTLIST", getPostUseCase().toString())
+        }
+    }
+
+
 }
